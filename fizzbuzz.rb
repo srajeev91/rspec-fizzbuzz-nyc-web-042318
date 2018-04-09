85c155fdfbc44fd6ee61dc0b0b6bf60362d60e9b
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-09 20:56:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++
@@ -1,9 +0,0 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
-  elsif number % 3 == 0
-    return "Fizz"
-  else number % 5 == 0
-    return "Buzz"
-  end
-end
